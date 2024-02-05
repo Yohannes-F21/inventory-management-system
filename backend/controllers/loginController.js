e50d3524c4a20c64faee991aea5a769e56798377
Yohannes Assefa <yohannes@acelabusa.com>
2024-02-05 20:22:10 +0300

    add login

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -50,6 +50,70 @@ const sendOTP = async (req, res) => {
         .json({ success: true, message: "OTP sent" })
 }
 
+const login = async (req, res) => {
+    const { email, otp } = req.body
+
+    if (!email || !otp) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ message: "Please provide email and otp" })
+    }
+
+    const user = await UserModel.findOne({ email })
+
+    if (!user) {
+        return res
+            .status(StatusCodes.UNAUTHORIZED)
+            .json({ message: "No Such User" })
+    }
+
+    const otpExists = await OTPModel.findOne({ email })
+
+    if (!otpExists) {
+        return res
+            .status(StatusCodes.UNAUTHORIZED)
+            .json({ message: "No such OTP sent for this account" })
+    }
+
+    if (!bcrypt.compare(otp, otpExists.otp)) {
+        return res
+            .status(StatusCodes.UNAUTHORIZED)
+            .json({ message: "Please Verify with valid OTP" })
+    }
+
+    const tokenUser = createTokenUser(user)
+
+    let refreshToken = ""
+
+    const existingToken = await TokenModel.findOne({ user: user._id })
+
+    if (existingToken) {
+        const { isValid } = existingToken
+
+        if (!isValid) {
+            return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json({ message: "Invalid token" })
+        }
+
+        refreshToken = existingToken.refreshToken
+        attachCookiesToResponse({ res, user: tokenUser, refreshToken })
+        res.status(StatusCodes.OK).json({ user: tokenUser })
+        return
+    }
+
+    refreshToken = crypto.randomBytes(40).toString("hex")
+    const userAgent = req.headers["user-agent"]
+    const ip = req.ip
+    const userToken = { refreshToken, ip, userAgent, user: user._id }
+
+    await TokenModel.create(userToken)
+    attachCookiesToResponse({ res, user: tokenUser, refreshToken })
+
+    res.status(StatusCodes.OK).json({ user: tokenUser })
+}
+
 module.exports = {
     sendOTP,
+    login,
 }
